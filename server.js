dcd2fe46e9079be13eaf94d0818eae18eb8d0114
Shah-12309 <worldiswonders@gmail.com>
2026-04-17 19:09:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -27,6 +27,9 @@ app.delete("/customers/:id", (req, res) => {
   res.json({ success: true });
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+
 });
