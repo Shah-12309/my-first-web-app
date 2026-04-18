d0453fa1583af5cf2bc9ad8071f1ad67169136ff
Shah-12309 <worldiswonders@gmail.com>
2026-04-18 17:40:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,54 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-let customers = [];
+/* 🔗 CONNECT MONGODB */
+mongoose.connect("mongodb+srv://admin:1234@cloudaws.5aqskkm.mongodb.net/?appName=CloudAWS/mydb")
+.then(() => console.log("MongoDB Connected ✅"))
+.catch(err => console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("Server is running 🚀");
+/* 📦 SCHEMA */
+const CustomerSchema = new mongoose.Schema({
+  name: String
 });
 
-app.get("/customers", (req, res) => {
+const Customer = mongoose.model("Customer", CustomerSchema);
+
+/* GET */
+app.get("/customers", async (req, res) => {
+  const customers = await Customer.find();
   res.json(customers);
 });
 
-app.post("/customers", (req, res) => {
-  const newCustomer = {
-    id: Date.now(),
+/* POST */
+app.post("/customers", async (req, res) => {
+  const newCustomer = new Customer({
     name: req.body.name
-  };
+  });
 
-  customers.push(newCustomer);
+  await newCustomer.save();
   res.json(newCustomer);
 });
 
-app.delete("/customers/:id", (req, res) => {
-  customers = customers.filter(c => c.id != req.params.id);
+/* PUT */
+app.put("/customers/:id", async (req, res) => {
+  const updated = await Customer.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name },
+    { new: true }
+  );
+
+  res.json(updated);
+});
+
+/* DELETE */
+app.delete("/customers/:id", async (req, res) => {
+  await Customer.findByIdAndDelete(req.params.id);
   res.json({ success: true });
 });
 
